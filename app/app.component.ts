faed8570645faf9537b5ffb064a3621c19eb6dd8
caackermann <cackermann@uc.cl>
2019-08-11 13:45:34 -0400

    ngifs

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-root',
+  selector:'app-root',
   styleUrls: ['app.component.scss'],
   template: `
-    <div class="app">
-      Hello!
+  <div class="app">
+    <button (click)="handleClick(username.value)">
+      Get Value
+    </button>
+    <input type="text" [(ngModel)]= "name" #username>
+    <div *ngIf="name.length">
+      Searching for... {{ name }}
     </div>
-  `
+  </div>
+  ` 
 })
 export class AppComponent {
-
+  name: string = ""
+  handleClick(value: string) {
+    console.log(value)
+  }
+  
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { AppComponent } from './app.component'
+import { CommonModule } from "@angular/common";
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [
-    AppComponent
-  ],
   declarations: [
-    AppComponent
-  ]
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
